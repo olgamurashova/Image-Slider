2a513561984e87194d02ca5739f31191ed7f50f6
Olga Murashova <om.dreyfus@gmail.com>
2022-10-01 18:10:07 -0400

    added images, changes style.css

--- main.js
+++ main.js
@@ -7,17 +7,34 @@ const imageContainer = document.querySelector(".image-container");
 const img = document.querySelectorAll("img");
 
 let currentImg = 1;
+let timeout;
+updateImg();
 
 next.addEventListener("click",()=>{
-    currentImg++
+    currentImg++;
+    clearTimeout(timeout);
     updateImg()
 
 });
 
+prev.addEventListener("click",()=>{
+    currentImg--;
+    clearTimeout(timeout);
+    updateImg();
+})
+
 function updateImg(){
     if(currentImg > img.legth){
-        currentImg =1;
+        currentImg = 1;
     }
-    imageContainer.style.transform = `translateX(-${(currentImg) * 500}px)`
+    else if(currentImg < 1) {
+        currentImg = img.length;
+    }
+    imageContainer.style.transform = `translateX(-${(currentImg) * 500}px)`;
+    timeout = setTimeout(()=> {
+        currentImg++;
+        updateImg();
+
+    }, 3000)
 
 }
